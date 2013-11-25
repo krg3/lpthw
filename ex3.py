96f3a25d214652e8a0e0e5c8447d603b7abc3874
Kyle Grablander <Kyle.Grablander@gmail.com>
2013-11-25 18:15:51 -0500

    ex1

---
+++ ex3.py
@@ -0,0 +1,9 @@
+print ("I will now count my chickens")
+
+print ("Hens",(25 + 30 / 6))
+print ("Roosters", (100 - 25 * 3 % 4))
+
+print ("I will now count the eggs")
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print (7.0 % 3.0)
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Kyle"
+my_age = 27
+my_height = 71
+my_weight = 150
+my_eyes = "blue"
+my_teeth = "white"
+my_hair = "brown"
+
+print ("Let's talk about %s." % my_name)
+print ("He's %d inches tall." % my_height)
+print ("He's %d pounds heavy." % my_weight)
+print ("Actually, that's not too heavy.")
+print ("He's got %s eyes and %s hair." % (my_eyes, my_hair))
+print ("His teeth are usually %s depending on the coffee." % my_teeth)
+
+#this line is tricky
+print ("If I add %d, and %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
